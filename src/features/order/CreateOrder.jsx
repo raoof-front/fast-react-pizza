f1b1e6ecfe9ba823f14f87893c732f3a1d9ca640
raouf <rauofallama@gmail.com>
2024-10-02 22:33:52 -0700

    Writing Data With React Router Action

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,10 +1,12 @@
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
   );
-  console.log(isValidPhone)
+console.log(isValidPhone)
 
 const fakeCart = [
   {
@@ -38,7 +40,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let{`'`}s go!</h2>
 
-      <form>
+      <Form method="POST" >
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -63,18 +65,31 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+          // value={withPriority}
+          // onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)}/>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+export async function action({request}) {
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  const order = {
+    ...data,
+    priority: data.priority === 'on',
+    cart: JSON.parse(data.cart)
+  }
+  const newOrder = await createOrder(order)
+  console.log(order)
+  return redirect(`/order/${newOrder.id}`)
 }
 
 export default CreateOrder;
